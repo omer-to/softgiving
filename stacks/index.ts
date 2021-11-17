b9c2a5a5d2757c33bb7ea65f4612a4de5d73a792
Ömer Toraman <omer.t@mindbehind.com>
2021-11-17 18:23:57 +0300

    Add DESTROY as the removal policy if it is test or dev

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,7 +1,11 @@
+import { RemovalPolicy } from "@aws-cdk/core";
 import * as sst from "@serverless-stack/resources";
 import { MainStack } from "./MainStack";
 
 export default function main(app: sst.App): void {
+      if (app.stage.includes('dev') || app.stage.includes('test')) {
+            app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY)
+      }
       // Set default runtime for all functions
       app.setDefaultFunctionProps({
             runtime: "nodejs14.x"
