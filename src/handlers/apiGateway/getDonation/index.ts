a34fc437035aadcf46c16cdccee8bcdabc12a6c6
Ömer Toraman <omer.t@mindbehind.com>
2021-11-17 18:23:57 +0300

    middfy getDonation handler

--- src/handlers/apiGateway/getDonation/index.ts
+++ src/handlers/apiGateway/getDonation/index.ts
@@ -1,16 +1,21 @@
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
+
+import { middyfy } from '../../../lib/apiGateway/middyfy'
 import { getDonationById } from '../../../lib/dynamoDB/getDonationById'
 
 type PathParameters = {
       transactionId: string
 }
 
-export const main: APIGatewayProxyHandlerV2 = async (evt) => {
+// @ts-expect-error
+export const handler: APIGatewayProxyHandlerV2 = async (evt) => {
       const { transactionId } = evt.pathParameters as PathParameters
       const donation = await getDonationById(transactionId)
 
       if (donation.Item) return (
-            { statusCode: 200, body: JSON.stringify(donation.Item) }
+            { statusCode: 200, body: donation.Item }
       )
       return { statusCode: 404, body: `Donation ${transactionId} does not exist` }
 }
+
+export const main = middyfy(handler, { useHttpJsonBodyParser: false })
