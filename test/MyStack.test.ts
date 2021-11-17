5a815e03dd0d441b70e80dccfefa6286cf59bcb0
Ömer Toraman <omer.t@mindbehind.com>
2021-11-17 18:29:05 +0300

    Rename stack

--- test/MyStack.test.ts
+++ test/MyStack.test.ts
@@ -1,11 +1,11 @@
 import { expect, haveResource } from "@aws-cdk/assert";
 import * as sst from "@serverless-stack/resources";
-import MyStack from "../stacks/MyStack";
+import { MainStack } from "../stacks/MainStack";
 
 test("Test Stack", () => {
-  const app = new sst.App();
-  // WHEN
-  const stack = new MyStack(app, "test-stack");
-  // THEN
-  expect(stack).to(haveResource("AWS::Lambda::Function"));
+      const app = new sst.App();
+      // WHEN
+      const stack = new MainStack(app, "main-stack");
+      // THEN
+      expect(stack).to(haveResource("AWS::Lambda::Function"));
 });
