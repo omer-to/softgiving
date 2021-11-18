f74a3ecd1b27ab0070e8e6c3ce84e2bb232783b7
Ömer Toraman <omer.t@mindbehind.com>
2021-11-18 08:23:54 +0300

    Unmarshall result

--- src/handlers/apiGateway/getUser/index.ts
+++ src/handlers/apiGateway/getUser/index.ts
@@ -1,3 +1,4 @@
+import { unmarshall } from '@aws-sdk/util-dynamodb'
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
 import { middyfy } from '../../../lib/apiGateway/middyfy'
 
@@ -6,14 +7,23 @@ import { getUserProfile } from '../../../lib/dynamoDB/getUserProfile'
 type PathParameters = {
       email: string
 }
-
 // @ts-expect-error
 export const handler: APIGatewayProxyHandlerV2 = async (evt) => {
       const { email } = evt.pathParameters as PathParameters
       const userProfile = await getUserProfile(email)
 
-      if (userProfile.Item) return { statusCode: 200, body: userProfile.Item }
-      else return { statusCode: 404, body: `No user with ${email} exists` }
+      if (userProfile.Item) {
+            const { amount } = unmarshall(userProfile.Item)
+            return {
+                  statusCode: 200,
+                  body: { amount }
+            }
+      }
+      return {
+            statusCode: 404,
+            headers: { 'Content-Type': 'text/plain' },
+            body: `No user with ${email} exists`
+      }
 }
 
 export const main = middyfy(handler, { useHttpJsonBodyParser: false })
