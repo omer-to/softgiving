34e27357823da2c840aff430cd25b40e6c378e49
Ömer Toraman <omer.t@mindbehind.com>
2021-11-18 08:23:35 +0300

    Implement getDonationsByUser

--- src/handlers/apiGateway/getDonationsByUser/index.ts
+++ src/handlers/apiGateway/getDonationsByUser/index.ts
@@ -1,3 +1,4 @@
+import { unmarshall } from '@aws-sdk/util-dynamodb'
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
 import { middyfy } from '../../../lib/apiGateway/middyfy'
 
@@ -7,9 +8,21 @@ type PathParameters = {
       email: string
 }
 
+// @ts-expect-error
 export const handler: APIGatewayProxyHandlerV2 = async (evt) => {
       const { email } = evt.pathParameters as PathParameters
-      return { body: 'donationsByUser' }
+      try {
+            const output = await getDonationsByUser(email)
+            const donations = output.Items?.map(item => unmarshall(item))
+            return {
+                  statusCode: 200,
+                  body: { donations }
+            }
+
+      } catch (error) {
+            console.error('error ocured')
+            console.error({ error })
+      }
 }
 
 export const main = middyfy(handler, { useHttpJsonBodyParser: false })
